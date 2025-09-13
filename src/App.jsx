3a3945a2f30fc7914c1d4ce8aa62ee4492bdc76a
Uroljon <ukhidirboev@gmail.com>
2025-09-13 20:50:31 +0200

    Code cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Outlet } from 'react-router-dom'
 import Header from './component/Header'
@@ -9,9 +6,8 @@ function App() {
 
   return (
     <>
-   <Header />
-     <Outlet />
-     {/* Footer\ */}
+      <Header />
+      <Outlet />
     </>
   )
 }
